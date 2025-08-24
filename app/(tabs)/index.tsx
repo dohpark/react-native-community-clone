005be17bd818b4c1cd0ad802dc3d5e8929bc388b
dohpark <dohpark1129@gmail.com>
2025-08-24 21:12:26 +0900

    feat: FeedList 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,10 @@
-import FeedItem from "@/components/FeedItem";
+import FeedList from "@/components/FeedList";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <FeedItem
-        post={{
-          id: 1,
-          userId: 1,
-          title: "더미 제목입니다.",
-          description:
-            "더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다.  더미 내용입니다.더미 내용입니다.  더미 내용입니다. 더미 내용입니다.",
-          createdAt: "",
-          author: {
-            id: 1,
-            nickname: "닉네임",
-            imageUri: "",
-          },
-          imageUris: [],
-          likes: [],
-          hasVote: false,
-          voteCount: 1,
-          commentCount: 1,
-          viewCount: 1,
-        }}
-      />
+      <FeedList />
     </SafeAreaView>
   );
 }
