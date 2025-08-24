fffc88031283037fd5584684c18a500acbd16b4b
dohpark <dohpark1129@gmail.com>
2025-08-24 21:14:44 +0900

    ui: homescreen에 backgroundColor 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
 import FeedList from "@/components/FeedList";
+import { colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <FeedList />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+});
