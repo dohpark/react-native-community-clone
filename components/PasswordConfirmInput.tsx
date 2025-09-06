c1decc18da96779f6374b78aa0490277c34bae26
dohpark <dohpark1129@gmail.com>
2025-09-06 17:38:14 +0900

    feat: react-hook-form을 활용하여 로그인 및 회원가입 페이지 적용

---
+++ components/PasswordConfirmInput.tsx
@@ -0,0 +1,21 @@
+import { Controller, useFormContext } from "react-hook-form";
+import InputField from "./InputField";
+
+export default function PasswordConfirmInput() {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name="passwordConfirm"
+      control={control}
+      render={({ field: { onChange, value } }) => (
+        <InputField
+          label="비밀번호 확인"
+          placeholder="비밀번호를 입력해주세요."
+          value={value}
+          onChangeText={onChange}
+        />
+      )}
+    />
+  );
+}
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,7 @@
 const colors = {
   WHITE: "#FFF",
+  RED_100: "#FFDFDF",
+  RED_500: "#FF5F5F",
   ORANGE_600: "#FF6B57",
   BLACK: "#000",
   GRAY_100: "#F6F6F6",
